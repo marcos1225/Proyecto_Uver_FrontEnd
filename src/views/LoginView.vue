1bd26095d85f72cd16f5f6a5c72b31b01e816484
Marcos <marcosgonzana18@gmail.com>
2024-06-28 09:52:11 -0600

    arreglados algunas redicciones

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <BottonsRegister
     passengerButtonText="Iniciar sesión como pasajero"
-    passengerButtonLink="/register-number-login?backLink=/login"
+    passengerButtonLink="/take-trip"
     driverButtonText="Iniciar sesión como conductor"
     driverButtonLink="/register-number-login?backLink=/login"
   />
